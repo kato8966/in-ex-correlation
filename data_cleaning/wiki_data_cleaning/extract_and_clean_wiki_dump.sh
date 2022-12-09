c8eeea43130cb69249898262b98f52060dab3ff9
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-09 09:15:55 +0900

    edit extract_and_clean_wiki_dump.sh

--- data_cleaning/wiki_data_cleaning/extract_and_clean_wiki_dump.sh
+++ data_cleaning/wiki_data_cleaning/extract_and_clean_wiki_dump.sh
@@ -4,13 +4,13 @@ set -e
 WIKI_DUMP_FILE_IN=$1
 WIKI_DUMP_FILE_OUT=${WIKI_DUMP_FILE_IN%%.*}.txt
 
-# clone the WikiExtractor repository
-git clone https://github.com/attardi/wikiextractor.git
+# clone a modified WikiExtractor repository
+git clone https://github.com/kato8966/wikiextractor.git
 cd wikiextractor
 
 # extract and clean the chosen Wikipedia dump
 echo "Extracting and cleaning $WIKI_DUMP_FILE_IN to $WIKI_DUMP_FILE_OUT..."
-python3 -m wikiextractor.WikiExtractor ../$WIKI_DUMP_FILE_IN --processes 8 -o - \
+python3 -m wikiextractor.WikiExtractor -o - -q ../$WIKI_DUMP_FILE_IN \
 | sed "/^\s*\$/d" \
 | grep -v "^<doc id=" \
 | grep -v "</doc>\$" \
