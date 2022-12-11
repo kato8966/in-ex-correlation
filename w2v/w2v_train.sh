3d22b1237bc1dbb2ae0b0a4ab060f2026434d67c
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-11 16:04:12 +0900

    edit w2v_train.sh

--- w2v/w2v_train.sh
+++ w2v/w2v_train.sh
@@ -1,32 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash -l
+#PJM -g gk77
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -m e
 
 ### COMMAND LINE ARGUMENTS
 # $1 = path to training data (text file)
 # $2 = path to file where embeddings should be saved (text file)
 
-export STUDENT_ID=$(whoami)
-
 # set it to fail at first error
 set -o errexit
 
-# source my bashrc
-source ~/.bashrc
-# activate the project environment
-conda activate bias_env
-
-echo Creating a folder in scratch space
-mkdir -p /disk/scratch/${STUDENT_ID}/w2v_temp
-
-echo Copying data over to scratch space
-# copy data over from headnode to scratch space
-rsync -av $1 /disk/scratch/${STUDENT_ID}/w2v_temp/training_data.txt
+module load miniconda
+source ${MINICONDA_DIR}/etc/profile.d/conda.sh
+conda activate in-ex-cor
 
 echo Executing python script
-python train_w2v_embeddings.py /disk/scratch/${STUDENT_ID}/w2v_temp/training_data.txt /disk/scratch/${STUDENT_ID}/w2v_temp/w2v_vectors_w2vformat.txt
-
-
-echo Copying models back to headnode
-rsync -av /disk/scratch/${STUDENT_ID}/w2v_temp/w2v_vectors_w2vformat.txt $2
-
-echo Deleting data and model from scratch space
-rm -r /disk/scratch/${STUDENT_ID}/w2v_temp/
+python train_w2v_embeddings.py $1 $2
