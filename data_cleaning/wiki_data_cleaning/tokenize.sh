f8bee46fd2f00ead4a4ef8358199fce5c4f019af
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-25 09:53:38 +0900

    edit tokenize.sh, run it & final_preprocess.py, & ignore the results

--- data_cleaning/wiki_data_cleaning/tokenize.sh
+++ data_cleaning/wiki_data_cleaning/tokenize.sh
@@ -4,7 +4,7 @@
 #PJM -m e
 #PJM -L rscgrp=share
 #PJM -L gpu=1
-#PJM -L elapse=48:00:00
+#PJM -L elapse=8:00:00
 set -o errexit
 
 module load miniconda
@@ -12,4 +12,4 @@ source ${MINICONDA_DIR}/etc/profile.d/conda.sh
 conda activate in-ex-cor
 
 echo Executing python script
-python tokenise_corpus.py enwiki-latest-pages-articles.txt enwiki-latest-pages-articles_tokenized.txt
+python tokenise_corpus.py enwiki-latest-pages-articles_preprocessed.txt enwiki-latest-pages-articles_tokenized.txt
