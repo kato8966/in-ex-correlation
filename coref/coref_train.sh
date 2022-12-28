6ca1293ecf35d92872d0cc281a1c09b3a685ea93
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-28 10:30:00 +0900

    coref: use the downloaded image

--- coref/coref_train.sh
+++ coref/coref_train.sh
@@ -18,4 +18,4 @@ module load singularity/3.9.5
 
 echo Training coreference model
 mkdir -p $HOME/.allennlp
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv docker://allennlp/models train ${coref_config_file} -s $1
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ${coref_config_file} -s $1
