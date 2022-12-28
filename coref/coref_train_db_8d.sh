2fd00f290dbf77557afde7cf4c5d288cd8adf023
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-28 11:18:39 +0900

    create coref_config_file & coref_train.sh for dataset balancing

---
+++ coref/coref_train_db_8d.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#PJM -g gk77
+#PJM -j
+#PJM -m e
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -L elapse=4:00:00
+#PJM -L jobenv=singularity
+
+## COMMAND LINE ARGUMENTS
+# $1: path to save folder for the results
+
+# set to fail at first error
+set -o errexit
+
+module load cuda/11.4
+module load singularity/3.9.5
+
+echo Training coreference model
+mkdir -p $HOME/.allennlp
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ./coref_config_file_db_8d -s ./model/db_8d
