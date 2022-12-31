8ff4d9ce5011ced3a504c24ecb3342f8aa65495a
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-31 10:21:41 +0900

    edit evaluate_allennlp.sh

--- coref/evaluate_allennlp.sh
+++ coref/evaluate_allennlp.sh
@@ -1,38 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash -l
+#PJM -g gk77
+#PJM -j
+#PJM -m e
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -L elapse=2:00:00
 
-## COMMAND LINE ARGUMENTS
-# $1: trained coreference resolution model (model.tar.gz)
-# $2: folder where the results will be saved
-
-export STUDENT_ID=$(whoami)
-
-# activate allennlp environment
-conda activate allennlp
-
-# create a folder
-mkdir -p /disk/scratch/${STUDENT_ID}/coref_evaluate
-
-# copy the winobias evaluation data into scratch space
-rsync -av ./evaluation_data/test_type1_anti_stereotype.v4_auto_conll /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type1_anti_stereotype.v4_auto_conll
-rsync -av ./evaluation_data/test_type1_pro_stereotype.v4_auto_conll /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type1_pro_stereotype.v4_auto_conll
-rsync -av ./evaluation_data/test_type2_anti_stereotype.v4_auto_conll /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type2_anti_stereotype.v4_auto_conll
-rsync -av ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type2_pro_stereotype.v4_auto_conll
-
-
-# copy the trained coreference model into scratch space
-rsync -av $1 /disk/scratch/${STUDENT_ID}/coref_evaluate/model.tar.gz
+MODEL=
+RESULT=
 
 # evaluate the model on the four different sets of evaluation data
-allennlp evaluate /disk/scratch/${STUDENT_ID}/coref_evaluate/model.tar.gz /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type1_anti_stereotype.v4_auto_conll --output-file /disk/scratch/${STUDENT_ID}/coref_evaluate/type1_anti_results.txt
-allennlp evaluate /disk/scratch/${STUDENT_ID}/coref_evaluate/model.tar.gz /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type1_pro_stereotype.v4_auto_conll --output-file /disk/scratch/${STUDENT_ID}/coref_evaluate/type1_pro_results.txt
-allennlp evaluate /disk/scratch/${STUDENT_ID}/coref_evaluate/model.tar.gz /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type2_anti_stereotype.v4_auto_conll --output-file /disk/scratch/${STUDENT_ID}/coref_evaluate/type2_anti_results.txt
-allennlp evaluate /disk/scratch/${STUDENT_ID}/coref_evaluate/model.tar.gz /disk/scratch/${STUDENT_ID}/coref_evaluate/test_type2_pro_stereotype.v4_auto_conll --output-file /disk/scratch/${STUDENT_ID}/coref_evaluate/type2_pro_results.txt
-
-# copy the results to the specified folder
-rsync -av /disk/scratch/${STUDENT_ID}/coref_evaluate/type1_anti_results.txt $2/type1_anti_results.txt
-rsync -av /disk/scratch/${STUDENT_ID}/coref_evaluate/type1_pro_results.txt $2/type1_pro_results.txt
-rsync -av /disk/scratch/${STUDENT_ID}/coref_evaluate/type2_anti_results.txt $2/type2_anti_results.txt
-rsync -av /disk/scratch/${STUDENT_ID}/coref_evaluate/type2_pro_results.txt $2/type2_pro_results.txt
-
-# remove data from scratch space
-rm -r /disk/scratch/${STUDENT_ID}/coref_evaluate
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type1_anti_stereotype.v4_auto_conll --output-file $RESULT/type1_anti_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type1_pro_stereotype.v4_auto_conll --output-file $RESULT/type1_pro_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type2_anti_stereotype.v4_auto_conll --output-file $RESULT/type2_anti_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll --output-file $RESULT/type2_pro_results.txt
