1192d0af78b30d1a69ecb9b31d1d4d76c10ea7a1
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2022-12-31 10:40:49 +0900

    edit evaluate_allennlp.sh

--- coref/evaluate_allennlp.sh
+++ coref/evaluate_allennlp.sh
@@ -5,12 +5,15 @@
 #PJM -L rscgrp=share
 #PJM -L gpu=1
 #PJM -L elapse=2:00:00
+#PJM -L jobenv=singularity
 
-MODEL=
-RESULT=
+module load cuda/11.4
+module load singularity/3.9.5
+
+NAME=
 
 # evaluate the model on the four different sets of evaluation data
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type1_anti_stereotype.v4_auto_conll --output-file $RESULT/type1_anti_results.txt
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type1_pro_stereotype.v4_auto_conll --output-file $RESULT/type1_pro_results.txt
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type2_anti_stereotype.v4_auto_conll --output-file $RESULT/type2_anti_results.txt
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate $MODEL ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll --output-file $RESULT/type2_pro_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type1_anti_stereotype.v4_auto_conll --output-file ./result/$NAME/type1_anti_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type1_pro_stereotype.v4_auto_conll --output-file ./result/$NAME/type1_pro_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type2_anti_stereotype.v4_auto_conll --output-file ./result/$NAME/type2_anti_results.txt
+singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll --output-file ./result/$NAME/type2_pro_results.txt
