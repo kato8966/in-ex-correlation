b4aaeee653d9f4305aa80d003fa179d3dd2e35f3
Katô Taisei <katotaisei@g.ecc.u-tokyo.ac.jp>
2023-05-14 17:48:19 +0900

    comply with the license terms of e2e-coref/setup_training.sh

--- ontonotes_converter.sh
+++ ontonotes_converter.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Original file: e2e-coref/setup_training.sh (https://github.com/kentonl/e2e-coref/blob/master/setup_training.sh)
+#   Copyright 2017 Kenton Lee
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   MODIFIED
+
 dlx() {
   wget $1/$2
   tar -xvzf $2
