b4cc5795bea6d8150be00b4cb06d0e80a016a071
Katô Taisei <katotaisei064@gmail.com>
2023-05-27 17:48:53 +0900

    make evaluate_allennlp_db_winobias.sh more portable, create pjsub_evaluate_allennlp_db_winobias.sh, run it, & ignore it

--- coref/evaluate_allennlp_db_winobias.sh
+++ coref/evaluate_allennlp_db_winobias.sh
@@ -1,14 +1,4 @@
-#!/bin/bash -l
-#PJM -g gk77
-#PJM -j
-#PJM -m e
-#PJM -L rscgrp=share
-#PJM -L gpu=1
-#PJM -L elapse=10:00:00
-
-module load miniconda
-source ${MINICONDA_DIR}/etc/profile.d/conda.sh
-conda activate allennlp
+#!/usr/bin/env sh
 
 for type in debias overbias
 do
