c1746c71dc42015849848d7aab74765dad88b72b
Katô Taisei <katotaisei064@gmail.com>
2023-06-13 17:30:49 +0900

    rename rnsb.* to rnsb_old.*

---
+++ RNSB/rnsb_old.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python rnsb_old.py ../w2v/original_vectors.txt result/original.txt
+
+for i in `seq 6 8`
+do
+    for f in d o
+    do
+        python rnsb_old.py ../w2v/db_$i${f}_vectors.txt result/db_$i$f.txt
+    done
+done
+
+for i in `seq 1 6`
+do
+    python rnsb_old.py ../attract-repel/ar_t${i}_w2v_format_vectors.txt result/ar_t$i.txt
+done
