10a08a4102ee326a565baace612a1a0b28081b3f
Katô Taisei <katotaisei064@gmail.com>
2023-06-13 17:22:22 +0900

    update evaluate_allennlp_winobias.sh

--- coref/evaluate_allennlp_winobias.sh
+++ coref/evaluate_allennlp_winobias.sh
@@ -1,17 +1,29 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+eval_allennlp ()
+{
+    # evaluate the model on the five different sets of evaluation data
+    mkdir ./result/$1
+    allennlp evaluate ./model/$1/model.tar.gz ../ontonotes-conll/test.english.v4_gold_conll --output-file ./result/$1/conll_results.txt
+    allennlp evaluate ./model/$1/model.tar.gz ./evaluation_data/test_type1_anti_stereotype.v4_auto_conll --output-file ./result/$1/type1_anti_results.txt
+    allennlp evaluate ./model/$1/model.tar.gz ./evaluation_data/test_type1_pro_stereotype.v4_auto_conll --output-file ./result/$1/type1_pro_results.txt
+    allennlp evaluate ./model/$1/model.tar.gz ./evaluation_data/test_type2_anti_stereotype.v4_auto_conll --output-file ./result/$1/type2_anti_results.txt
+    allennlp evaluate ./model/$1/model.tar.gz ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll --output-file ./result/$1/type2_pro_results.txt
+}
 
 for type in debias overbias
 do
-    for ratio in 0.0 0.2 0.4 0.6 0.8
+    for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
     do
-        NAME=db_winobias_${type}_$ratio
-
-        # evaluate the model on the five different sets of evaluation data
-        mkdir ./result/$NAME
-        allennlp evaluate ./model/$NAME/model.tar.gz ../ontonotes-conll/test.english.v4_gold_conll --output-file ./result/$NAME/conll_results.txt
-        allennlp evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type1_anti_stereotype.v4_auto_conll --output-file ./result/$NAME/type1_anti_results.txt
-        allennlp evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type1_pro_stereotype.v4_auto_conll --output-file ./result/$NAME/type1_pro_results.txt
-        allennlp evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type2_anti_stereotype.v4_auto_conll --output-file ./result/$NAME/type2_anti_results.txt
-        allennlp evaluate ./model/$NAME/model.tar.gz ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll --output-file ./result/$NAME/type2_pro_results.txt
+        eval_allennlp db_winobias_${type}_$ratio
     done
+done
+
+for t in `seq 36`
+do
+    eval_allennlp ar_winobias_t$t &
+    if [[ t%9 -eq 0 ]]
+    then
+        wait
+    fi
 done
