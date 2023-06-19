0f3249ea607738f9a5967874c94b8b48e5ce419c
Katô Taisei <katotaisei064@gmail.com>
2023-06-19 13:30:23 +0900

    weat: receive additional argument for wordlist

--- WEAT/wefe-weat.py
+++ WEAT/wefe-weat.py
@@ -2,18 +2,19 @@ import sys
 
 from gensim.models import KeyedVectors
 from wefe.metrics import WEAT
-from wefe.query import Query
 from wefe.word_embedding_model import WordEmbeddingModel
 
 import wordlists
 
 # command line argument
 # $1 = path to word embedding file in GloVe format
-# $2 = path to a file where the result will be saved
+# $2 = wordlist used
+# $3 = path to a file where the result will be saved
 
 vecs = KeyedVectors.load_word2vec_format(sys.argv[1], no_header=True)
 model = WordEmbeddingModel(vecs, "glove")
 weat = WEAT()
-with open(sys.argv[2], "w") as fout:
-    result = weat.run_query(wordlists.winobias(), model, lost_vocabulary_threshold=0.0)
+with open(sys.argv[3], "w") as fout:
+    result = weat.run_query(getattr(wordlists, sys.argv[2])(), model,
+                            lost_vocabulary_threshold=0.0)
     fout.write(result)
