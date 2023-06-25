1c4de6effc22c2642a28cff5cc2a0ac9ae182463
Katô Taisei <katotaisei064@gmail.com>
2023-06-25 19:02:55 +0900

    edit attract-repel_winobias.sh

--- attract-repel/attract-repel_winobias.sh
+++ attract-repel/attract-repel_winobias.sh
@@ -3,14 +3,15 @@
 i=1
 for type in debias overbias
 do
-    for reg in 1e-8 1e-9 1e-10
+    for reg in 1e-1 5e-2
     do
-        for sim in 0.5 0.6 0.7
+        for sim in 0.0 0.5 1.0
         do
-            for ant in 0.0 0.1
+            for ant in 0.0 0.5 1.0
             do
                 python attract-repel_new_pytorch.py experiment_parameters/winobias_${type}_reg${reg}_sim${sim}_ant${ant}.cfg &
-                if [ $(($i % 2)) -eq 0 ] ; then
+                if [[ i%2 -eq 0 ]]
+                then
                     wait
                 fi
                 ((i++))
