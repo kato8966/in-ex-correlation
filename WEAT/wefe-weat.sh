aaa767736e33ca46db5883b522ea263b7288bf45
Katô Taisei <katotaisei064@gmail.com>
2023-06-26 09:15:27 +0900

    edit wefe-weat.sh

--- WEAT/wefe-weat.sh
+++ WEAT/wefe-weat.sh
@@ -1,28 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-python wefe-weat.py ../w2v/original_lc_vectors.txt result/original_lc.txt
+python wefe-weat.py ../w2v/vectors/original_lc_vectors.txt winobias result/original_lc.txt &
 
+i=1
 for type in debias overbias
 do
     for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
     do
         temp=db_winobias_${type}_${ratio}
-        python wefe-weat.py ../w2v/${temp}_vectors.txt result/$temp.txt
+        python wefe-weat.py ../w2v/vectors/${temp}_vectors.txt winobias result/$temp.txt &
+        if [[ i%6 -eq 0 ]]
+        then
+            wait
+        fi
+        ((i++))
     done
 done
+wait
 
 i=1
 for type in debias overbias
 do
-    for reg in 1e-8 1e-9 1e-10
+    for reg in 1e-1 5e-2
     do
-        for sim in 0.5 0.6 0.7
+        for sim in 0.0 0.5 1.0
         do
-            for ant in 0.0 0.1
+            for ant in 0.0 0.5 1.0
             do
                 temp=winobias_${type}_reg${reg}_sim${sim}_ant${ant}
-                python wefe-weat.py ../attract-repel/vectors/${temp}_vectors.txt result/ar_$temp.txt &
-                if [ $(($i % 6)) -eq 0 ]
+                python wefe-weat.py ../attract-repel/vectors/${temp}_vectors.txt winobias result/ar_$temp.txt &
+                if [[ i%6 -eq 0 ]]
                 then
                     wait
                 fi
