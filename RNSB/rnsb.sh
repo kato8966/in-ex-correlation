47c610a6be8fd2c292cc9667958bafc095b9af21
Katô Taisei <katotaisei064@gmail.com>
2023-07-01 12:23:11 +0900

    edit rnsb.sh

--- RNSB/rnsb.sh
+++ RNSB/rnsb.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
-python rnsb.py ../w2v/vectors/original_lc_vectors.txt winobias result/original_lc.txt &
-
-for t in debias overbias
+for wordlist in winobias winobias_rev
 do
-    for i in `seq 0 9`
+    python rnsb.py ../w2v/vectors/original_lc_vectors.txt $wordlist result/original_lc_$wordlist.txt &
+
+    for t in debias overbias
     do
-        sample_prob=0.$i
-        temp=db_winobias_${t}_$sample_prob
-        python rnsb.py ../w2v/vectors/${temp}_vectors.txt winobias result/$temp.txt &
-    done
-    wait
-done
-
-for t in `seq 36`
-do
-    temp=ar_winobias_t$t
-    python rnsb.py ../attract-repel/vectors/${temp}_vectors.txt winobias result/$temp.txt &
-    if [[ t%9 -eq 0 ]]
-    then
+        for i in `seq 0 9`
+        do
+            sample_prob=0.$i
+            python rnsb.py ../w2v/vectors/db_winobias_${t}_${sample_prob}_vectors.txt $wordlist result/db_${wordlist}_${t}_$sample_prob.txt &
+        done
         wait
-    fi
+    done
+
+    t=1
+    for typ in debias overbias
+    do
+        for reg in 1e-1 5e-2
+        do
+            for sim in 0.0 0.5 1.0
+            do
+                for ant in 0.0 0.5 1.0
+                do
+                    python rnsb.py ../attract-repel/vectors/winobias_${typ}_reg${reg}_sim${sim}_ant${ant}_vectors.txt $wordlist result/ar_${wordlist}_${typ}_reg${reg}_sim${sim}_ant${ant}.txt &
+                    if [[ t%9 -eq 0 ]]
+                    then
+                        wait
+                    fi
+                    ((t++))
+                done
+            done
+        done
+    done
 done
