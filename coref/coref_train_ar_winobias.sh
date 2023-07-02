471aae7afecaa413bea451ea0d339cb16c36d918
Katô Taisei <katotaisei064@gmail.com>
2023-07-02 15:56:38 +0900

    edit coref_train_ar_winobias.sh

--- coref/coref_train_ar_winobias.sh
+++ coref/coref_train_ar_winobias.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# set to fail at first error
-set -o errexit
-
 echo Training coreference model
 mkdir -p $HOME/.allennlp
 
 for type in debias overbias
 do
-    for reg in 1e-8 1e-9 1e-10
+    for reg in 1e-1 5e-2 1e-2
     do
-        for sim in 0.5 0.6 0.7
+        for sim in 0.0 0.5 1.0
         do
-            for ant in 0.0 0.1
+            for ant in 0.0 0.5 1.0
             do
                 temp=ar_winobias_${type}_reg${reg}_sim${sim}_ant${ant}
+                mkdir model/$temp
                 singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ./coref_config/$temp -s ./model/$temp
             done
         done
