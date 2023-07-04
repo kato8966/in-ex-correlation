2c3492f8d37d0730f086b1f006d01ccae10ca26b
Katô Taisei <katotaisei064@gmail.com>
2023-07-04 09:26:20 +0900

    format_results.py: ignore RNSB & add new attract-repel results

--- format_results.py
+++ format_results.py
@@ -5,7 +5,7 @@ import math
 import os
 
 # coref
-HEADERS = ['name', 'winobias_weat_es', 'winobias_rnsb', 'winobias(rev)_rnsb']\
+HEADERS = ['name', 'winobias_weat_es']\
             + [f'type{typ}_{metric}_diff'
                for typ in [1, 2] for metric in ['precision', 'recall', 'f1']]\
             + ['CoNLL F1']
@@ -19,22 +19,13 @@ with open('results/coref.csv', 'w') as csvout:
                                      0.8, 0.9]]\
               + [f'ar_winobias_{typ}_reg{reg}_sim{sim}_ant{ant}'
                  for typ in ['debias', 'overbias']
-                 for reg in ['1e-1', '5e-2']
+                 for reg in ['1e-1', '5e-2', '1e-2']
                  for sim in [0.0, 0.5, 1.0] for ant in [0.0, 0.5, 1.0]]
     for name in names:
         with open(os.path.join('WEAT', 'result', name + '.txt')) as weatin:
             weat = json.load(weatin)['effect_size']
 
-        rnsb = {}
-        for wordlist in ['winobias', 'winobias_rev']:
-            if name == 'original_lc':
-                fname = f'original_lc_{wordlist}.txt'
-            else:
-                fname = name.replace('winobias', wordlist) + '.txt'
-            with open(os.path.join('RNSB', 'result', fname)) as rnsbin:
-                rnsb[wordlist] = float(rnsbin.readline())
-
-        if math.isnan(weat) or any(math.isnan(rnsb[wordlist]) for wordlist in ['winobias', 'winobias_rev']):
+        if math.isnan(weat):
             logging.warning(f'{name} has nan value, thus skipped.')
             continue
 
@@ -57,8 +48,7 @@ with open('results/coref.csv', 'w') as csvout:
                   = coref[f'type{typ}_pro_{metric}']\
                       - coref[f'type{typ}_anti_{metric}']
         
-        csv_writer.writerow([name, weat, rnsb['winobias'],
-                             rnsb['winobias_rev']]
+        csv_writer.writerow([name, weat]
                               + [coref[f'type{typ}_{metric}_diff']
                                  for typ in [1, 2]
                                  for metric in ['precision', 'recall', 'f1']]
