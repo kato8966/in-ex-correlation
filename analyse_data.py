2cf05c95fdd7742dd4d88bd936136bb885fc2470
Katô Taisei <katotaisei064@gmail.com>
2023-07-09 17:02:41 +0900

    analyse_data.py: handle 10 original_lc results

--- analyse_data.py
+++ analyse_data.py
@@ -1,5 +1,7 @@
 import csv
 import os
+from statistics import mean, stdev
+
 import matplotlib.pyplot as plt
 from scipy.stats import spearmanr
 
@@ -10,15 +12,32 @@ with open('results/coref.csv', newline='') as csvin:
     extrinsic_metrics = [f'type{typ}_{metric}_diff'
                          for typ in [1, 2]
                          for metric in ['precision', 'recall', 'f1']]
+    result = {}
     intrinsics = {metric: [] for metric in intrinsic_metrics}
     extrinsics = {metric: [] for metric in extrinsic_metrics}
     for row in csvreader:
-        for metric in intrinsic_metrics:
-            intrinsics[metric].append(float(row[metric]))
-        for metric in extrinsic_metrics:
-            extrinsics[metric].append(float(row[metric]))
+        result[row['name']] = {k: float(v) for k, v in row.items() if k != 'name'}
 
 assert len(intrinsic_metrics) == 1
+
+result['original_lc'] = {}
+original_lcs = [result[f'original_lc{i}'] for i in range(1, 11)]
+original_lc_stdev = {}
+for metric in intrinsic_metrics:
+    result['original_lc'][metric] = original_lcs[0][metric]
+for metric in extrinsic_metrics:
+    samples = [original_lc[metric] for original_lc in original_lcs]
+    result['original_lc'][metric] = mean(samples)
+    original_lc_stdev[metric] = stdev(samples)
+for i in range(1, 11):
+    result.pop(f'original_lc{i}')
+
+for data in result.values():
+    for metric in intrinsic_metrics:
+        intrinsics[metric].append(float(data[metric]))
+    for metric in extrinsic_metrics:
+        extrinsics[metric].append(float(data[metric]))
+
 fig_all, axs_all = plt.subplots(len(extrinsic_metrics))
 for i, intrinsic_metric in enumerate(intrinsic_metrics):
     for j, extrinsic_metric in enumerate(extrinsic_metrics):
@@ -27,7 +46,10 @@ for i, intrinsic_metric in enumerate(intrinsic_metrics):
         axs_all[j].scatter(intrinsic, extrinsic)
 
         fig, ax = plt.subplots()
-        ax.scatter(intrinsic, extrinsic)
+        ax.errorbar(intrinsic, extrinsic,
+                    yerr=[0.0] * (len(extrinsic) - 1)\
+                           + [original_lc_stdev[extrinsic_metric]],
+                    fmt='o')
         ax.set_title(f'{intrinsic_metric} v. {extrinsic_metric}')
         ax.set_xlabel(intrinsic_metric)
         ax.set_ylabel(extrinsic_metric)
