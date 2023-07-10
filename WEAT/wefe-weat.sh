0a336b6c4d7c46e3363d1b7f4e0e11fe93c68dd2
Katô Taisei <katotaisei064@gmail.com>
2023-07-10 17:49:08 +0900

    wefe-weat.sh: measure WEAT with WEAT 7 too

--- WEAT/wefe-weat.sh
+++ WEAT/wefe-weat.sh
@@ -1,39 +1,42 @@
 #!/usr/bin/env bash
 
-python wefe-weat.py ../w2v/vectors/original_lc_vectors.txt winobias result/original_lc.txt &
-
-i=1
-for type in debias overbias
+for wordset in winobias weat7
 do
-    for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
-    do
-        temp=db_winobias_${type}_${ratio}
-        python wefe-weat.py ../w2v/vectors/${temp}_vectors.txt winobias result/$temp.txt &
-        if [[ i%6 -eq 0 ]]
-        then
-            wait
-        fi
-        ((i++))
-    done
-done
-wait
+    python wefe-weat.py ../w2v/vectors/original_lc_vectors.txt $wordset result/original_lc_$wordset.txt &
 
-i=1
-for type in debias overbias
-do
-    for reg in 1e-1 5e-2 1e-2
+    i=1
+    for type in debias overbias
     do
-        for sim in 0.0 0.5 1.0
+        for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
         do
-            for ant in 0.0 0.5 1.0
+            temp=db_${wordset}_${type}_${ratio}
+            python wefe-weat.py ../w2v/vectors/${temp}_vectors.txt $wordset result/$temp.txt &
+            if [[ i%6 -eq 0 ]]
+            then
+                wait
+            fi
+            ((i++))
+        done
+    done
+    wait
+
+    i=1
+    for type in debias overbias
+    do
+        for reg in 1e-1 5e-2 1e-2
+        do
+            for sim in 0.0 0.5 1.0
             do
-                temp=winobias_${type}_reg${reg}_sim${sim}_ant${ant}
-                python wefe-weat.py ../attract-repel/vectors/${temp}_vectors.txt winobias result/ar_$temp.txt &
-                if [[ i%6 -eq 0 ]]
-                then
-                    wait
-                fi
-                ((i++))
+                for ant in 0.0 0.5 1.0
+                do
+                    temp=${wordset}_${type}_reg${reg}_sim${sim}_ant${ant}
+                    python wefe-weat.py ../attract-repel/vectors/${temp}_vectors.txt $wordset result/ar_$temp.txt &
+                    if [[ i%6 -eq 0 ]]
+                    then
+                        wait
+                    fi
+                    ((i++))
+                done
             done
         done
     done
