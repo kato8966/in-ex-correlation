c45d4e19adb6c219ebdf58311ab707d600aa1de5
Katô Taisei <katotaisei064@gmail.com>
2023-07-12 16:18:38 +0900

    parallelize dataset_balancing.sh more

--- dataset_balancing.sh
+++ dataset_balancing.sh
@@ -8,7 +8,7 @@ do
         for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
         do
             python dataset_balancing.py data_cleaning/wiki_data_cleaning/enwiki-latest-pages-articles_tokenized_lc.txt data_cleaning/wiki_data_cleaning/enwiki-latest-pages-articles_tokenized_lc_${wordlist}_${type}_${ratio}.txt $wordlist $type $ratio &
-            if [[ i%3 -eq 0 ]]
+            if [[ i%4 -eq 0 ]]
             then
                 wait
             fi
@@ -16,4 +16,3 @@ do
         done
     done
 done
-wait
