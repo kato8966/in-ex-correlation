dbb3013494a49fdc039b70cccf0cdc8a8f37fc1c
Katô Taisei <katotaisei064@gmail.com>
2023-07-12 16:14:30 +0900

    make w2v_train.sh cleaner

--- w2v/w2v_train.sh
+++ w2v/w2v_train.sh
@@ -2,8 +2,9 @@
 
 for t in debias overbias
 do
-    for sample_prob in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+    for counter in $(seq 0 9)
     do
+        sample_prob=0.$counter
         echo Executing python script
         python train_w2v_embeddings.py ../data_cleaning/wiki_data_cleaning/enwiki-latest-pages-articles_tokenized_lc_winobias_${t}_${sample_prob}_final.txt vectors/db_winobias_${t}_${sample_prob}_vectors.txt
     done
