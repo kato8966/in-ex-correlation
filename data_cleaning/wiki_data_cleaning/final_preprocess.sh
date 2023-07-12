0d6269711d73e3af3802793f5993d55a01efebbb
Katô Taisei <katotaisei064@gmail.com>
2023-07-12 13:21:55 +0900

    edit final_preprocess.sh for weat7 experiment

--- data_cleaning/wiki_data_cleaning/final_preprocess.sh
+++ data_cleaning/wiki_data_cleaning/final_preprocess.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-for type in debias overbias
+for wordlist in winobias weat7
 do
-    for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+    for type in debias overbias
     do
-        python final_preprocess.py enwiki-latest-pages-articles_tokenized_lc_winobias_${type}_${ratio}.txt
+        for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+        do
+            python final_preprocess.py enwiki-latest-pages-articles_tokenized_lc_${wordlist}_${type}_${ratio}.txt
+        done
     done
 done
