e10b8d69590d8a2959df19c3c35631f85a20c4ca
Katô Taisei <katotaisei064@gmail.com>
2023-07-12 16:23:33 +0900

    edit w2v_train.sh for WEAT7 experiment

--- w2v/w2v_train.sh
+++ w2v/w2v_train.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-for t in debias overbias
+for wordlist in winobias weat7
 do
-    for counter in $(seq 0 9)
+    for t in debias overbias
     do
-        sample_prob=0.$counter
-        echo Executing python script
-        python train_w2v_embeddings.py ../data_cleaning/wiki_data_cleaning/enwiki-latest-pages-articles_tokenized_lc_winobias_${t}_${sample_prob}_final.txt vectors/db_winobias_${t}_${sample_prob}_vectors.txt
+        for counter in $(seq 0 9)
+        do
+            sample_prob=0.$counter
+            echo Executing python script
+            python train_w2v_embeddings.py ../data_cleaning/wiki_data_cleaning/enwiki-latest-pages-articles_tokenized_lc_${wordlist}_${t}_${sample_prob}_final.txt vectors/db_${wordlist}_${t}_${sample_prob}_vectors.txt
+        done
     done
 done
