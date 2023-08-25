2b59fe26cad7e7e537ae0036653136a8d7124b65
Katô Taisei <katotaisei064@gmail.com>
2023-08-25 14:56:57 +0900

    merge train_w2v_embeddings.py, w2v_train_original_lc.sh & w2v_train.sh; rename output files

--- w2v/train_w2v_embeddings.py
+++ w2v/train_w2v_embeddings.py
@@ -1,39 +1,47 @@
-from gensim import utils
 import gensim.models
-import sys
+from multiprocessing import Pool
+from os import path
 
 
-## Construct an iterator to iterate over lines in the data and yield just one line at a time
-## Data passed in as first command line argument
-class MyIter(object):
-    """
-    An iterator that yields documents (where corpus contains one document per line). Yields lists of str.
-    """
-    def __iter__(self):
-        path = sys.argv[1]
-        with utils.open(path, 'r', encoding='utf-8') as fin:
-            for line in fin:
-                yield line.split()
-
-
-def main():
+def main(corpus_file, save_file):
     print('Instantiating the model')
     # instantiate the skipgram model
     model = gensim.models.Word2Vec(vector_size=300, window=5, min_count=5, workers=9,sg=1)
     print('Building the vocabulary')
     # build the vocabulary from the sentences yielded by the iterator
-    model.build_vocab(corpus_iterable=MyIter())
+    model.build_vocab(corpus_file=corpus_file)
     total_examples = model.corpus_count
     print('Training the model')
-    model.train(corpus_iterable=MyIter(), total_examples=total_examples, epochs=model.epochs, total_words=model.corpus_total_words)
+    model.train(corpus_file=corpus_file, total_examples=total_examples, epochs=model.epochs, total_words=model.corpus_total_words)
 
     # Save file passed as second argument
     print('Saving model to specified filepath')
-    save_file = sys.argv[2]
     # model saved in GloVe format
     model.wv.save_word2vec_format(save_file, write_header=False)
 
 
 
 if __name__ == '__main__':
-    main()
+    with Pool(2) as pool:
+        pool.starmap(main,
+                     [('../data_cleaning/wiki_data_cleaning/'
+                       'enwiki-latest-pages-articles_tokenized_lc_final.txt',
+                       'vectors/wikipedia.txt')] +
+                     [('../data_cleaning/wiki_data_cleaning/'
+                       f'enwiki-latest-pages-articles_tokenized_lc_{wordlist}_{bias_type}_0.{i}_final.txt',  # noqa: E501
+                       path.join('vectors',
+                                 f'wikipedia_db_{wordlist}_{bias_type}_0.{i}.txt'))  # noqa: E501
+                      for wordlist in ['winobias', 'weat7']
+                      for bias_type in ['debias', 'overbias']
+                      for i in range(10)])
+
+    with Pool(41) as pool:
+        pool.starmap(main,
+                     [('../data_cleaning/twitter_data_cleaning_en/stream/2017/'
+                       '04/processed.txt', 'vectors/twitter.txt')] +
+                     [('../data_cleaning/twitter_data_cleaning_en/stream/2017/'
+                       f'04/processed_{wordlist}_{bias_type}_0.{i}.txt',
+                       f'vectors/twitter_db_{wordlist}_{bias_type}_0.{i}.txt')
+                      for wordlist in ['hatespeech', 'weat8']
+                      for bias_type in ['debias', 'overbias']
+                      for i in range(10)])
