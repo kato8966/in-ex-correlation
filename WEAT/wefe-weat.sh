539d1c6d8cb0f746332cf14428c6d77054ba464e
Katô Taisei <katotaisei064@gmail.com>
2023-09-01 16:41:18 +0900

    edit wefe-weat.sh

--- WEAT/wefe-weat.sh
+++ WEAT/wefe-weat.sh
@@ -2,15 +2,15 @@
 
 for wordset in winobias weat7
 do
-    python wefe-weat.py ../w2v/vectors/original_lc_vectors.txt $wordset result/original_lc_$wordset.txt &
+    python wefe-weat.py ../w2v/vectors/wikipedia.txt $wordset result/wikipedia_w2v_$wordset.txt &
 
     i=1
     for type in debias overbias
     do
-        for ratio in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+        for ratio in $(seq 0.0 0.1 0.9)
         do
-            temp=db_${wordset}_${type}_${ratio}
-            python wefe-weat.py ../w2v/vectors/${temp}_vectors.txt $wordset result/$temp.txt &
+            temp=wikipedia_w2v_db_${wordset}_${type}_${ratio}
+            python wefe-weat.py ../w2v/vectors/${temp#w2v_}.txt $wordset result/$temp.txt &
             if [[ i%6 -eq 0 ]]
             then
                 wait
@@ -29,8 +29,8 @@ do
             do
                 for ant in 0.0 0.5 1.0
                 do
-                    temp=${wordset}_${type}_reg${reg}_sim${sim}_ant${ant}
-                    python wefe-weat.py ../attract-repel/vectors/${temp}_vectors.txt $wordset result/ar_$temp.txt &
+                    temp=wikipedia_w2v_ar_${wordset}_${type}_reg${reg}_sim${sim}_ant${ant}
+                    python wefe-weat.py ../attract-repel/vectors/${temp#ar_}.txt $wordset result/$temp.txt &
                     if [[ i%6 -eq 0 ]]
                     then
                         wait
