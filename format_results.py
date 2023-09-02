9aad493784b3af4f948a5e75766bb6a4a0a384a1
Katô Taisei <katotaisei064@gmail.com>
2023-09-02 10:39:46 +0900

    edit format_results.py

--- format_results.py
+++ format_results.py
@@ -26,9 +26,9 @@ for wordlist in wordlists:
                      for sim in [0.0, 0.5, 1.0] for ant in [0.0, 0.5, 1.0]]
         for name in names:
             if 'original_lc' in name:
-                weat_file_name = f'original_lc_{wordlist}'
+                weat_file_name = f'wikipedia_w2v_{wordlist}'
             else:
-                weat_file_name = name
+                weat_file_name = f'wikipedia_w2v_{name}'
             with open(os.path.join('WEAT', 'result',
                                    weat_file_name + '.txt')) as weatin:
                 weat = json.load(weatin)['effect_size']
@@ -38,12 +38,12 @@ for wordlist in wordlists:
                 continue
 
             coref = {}
-            with open(os.path.join('coref', 'result', name,
+            with open(os.path.join('coref', 'result', f'w2v_{name}',
                                    'conll_results.txt')) as corefin:
                 coref['conll_f1'] = json.load(corefin)['coref_f1']
             for typ in [1, 2]:
                 for ap in ['anti', 'pro']:
-                    with open(os.path.join('coref', 'result', name,
+                    with open(os.path.join('coref', 'result', f'w2v_{name}',
                                            f'type{typ}_{ap}_results.txt')) as corefin:  # noqa: E501
                         result = json.load(corefin)
                         for metric in ['precision', 'recall', 'f1']:
