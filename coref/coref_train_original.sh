eb26e5694418cba930693397b25ff04285362059
Katô Taisei <katotaisei064@gmail.com>
2023-09-06 20:11:04 +0900

    edit coref_train_original.sh

--- coref/coref_train_original.sh
+++ coref/coref_train_original.sh
@@ -1,21 +1,9 @@
-#!/bin/bash -l
-#PJM -g gk77
-#PJM -j
-#PJM -m e
-#PJM -L rscgrp=share
-#PJM -L gpu=1
-#PJM -L elapse=4:00:00
-#PJM -L jobenv=singularity
-
-## COMMAND LINE ARGUMENTS
-# $1: path to location where resulting model should be saved
-
-# set to fail at first error
-set -o errexit
-
-module load cuda/11.4
-module load singularity/3.9.5
+#!/bin/bash
 
 echo Training coreference model
 mkdir -p $HOME/.allennlp
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv docker://allennlp/models train ./coref_config_file_original -s ./model/original
+
+i=1
+for i in $(seq 10):
+    mkdir model/w2v_original$i
+    singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ./coref_config/w2v_original -s ./model/w2v_original$i
