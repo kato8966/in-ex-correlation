6204bed8187590253eb38c35a299af62fbf30925
Katô Taisei <katotaisei064@gmail.com>
2023-09-08 11:49:36 +0900

    parallelize train_ft_embeddings.py

--- fasttext/train_ft_embeddings.py
+++ fasttext/train_ft_embeddings.py
@@ -1,40 +1,37 @@
 from gensim.models.fasttext import FastText as FT_gensim
-from gensim import utils
-import tempfile
-import sys
+from multiprocessing import Pool
+from os import path
 
 
-## Construct an iterator to iterate over lines in the data and yield just one line at a time
-## Data passed in as first command line argument
-class MyIter(object):
-    """
-    An iterator that yields documents (where corpus contains one document per line). Yields lists of str.
-    """
-    def __iter__(self):
-        path = sys.argv[1]
-        with utils.open(path, 'r', encoding='utf-8') as fin:
-            for line in fin:
-                yield line.split()
-
-
-def main():
+def main(corpus_file, save_file):
     print('Instantiating the model')
     # instantiate the skipgram model
-    model = FT_gensim(size=300, window=5, min_count=5, sg=1)
+    model = FT_gensim(vector_size=300, window=5, min_count=5, workers=9,sg=1)
     print('Building the vocabulary')
     # build the vocabulary from the sentences yielded by the iterator
-    model.build_vocab(sentences=MyIter())
+    model.build_vocab(corpus_file=corpus_file)
     total_examples = model.corpus_count
     print('Training the model')
     # train the model
-    model.train(sentences=MyIter(), total_examples=total_examples, epochs=model.epochs, total_words=model.corpus_total_words)
+    model.train(corpus_file=corpus_file, total_examples=total_examples, epochs=model.epochs, total_words=model.corpus_total_words)
 
-    ## Save file passed in as second argument
+    # Save file passed as second argument
     print('Saving the model to specified filepath')
-    save_file = sys.argv[2]
-    # model saved in the w2v format, i.e. the first line contains <no. of words> <no. of dimensions>
-    model.wv.save_word2vec_format(save_file)
+    # model saved in GloVe format
+    model.wv.save_word2vec_format(save_file, write_header=False)
+
 
 
 if __name__ == '__main__':
-    main()
+    with Pool(8) as pool:
+        pool.starmap(main,
+                     [('../data_cleaning/wiki_data_cleaning/'
+                       'enwiki-latest-pages-articles_tokenized_lc_final.txt',
+                       'vectors/wikipedia.txt')] +
+                     [('../data_cleaning/wiki_data_cleaning/'
+                       f'enwiki-latest-pages-articles_tokenized_lc_final_{wordlist}_{bias_type}_0.{i}.txt',  # noqa: E501
+                       path.join('vectors',
+                                 f'wikipedia_db_{wordlist}_{bias_type}_0.{i}.txt'))  # noqa: E501
+                      for wordlist in ['winobias', 'weat7']
+                      for bias_type in ['debias', 'overbias']
+                      for i in range(10)])
