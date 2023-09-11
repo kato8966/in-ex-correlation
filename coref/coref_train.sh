f9e56149274737b76bed60f4315ca253f6087f71
Katô Taisei <katotaisei064@gmail.com>
2023-09-11 10:17:16 +0900

    organize coref_train

--- coref/coref_train.sh
+++ coref/coref_train.sh
@@ -1,21 +1,53 @@
-#!/bin/bash -l
-#PJM -g gk77
-#PJM -j
-#PJM -m e
-#PJM -L rscgrp=share
-#PJM -L gpu=1
-#PJM -L elapse=4:00:00
-#PJM -L jobenv=singularity
+#!/bin/bash
 
-## COMMAND LINE ARGUMENTS
-# $1: path to save folder for the results
-
-# set to fail at first error
-set -o errexit
-
-module load cuda/11.4
-module load singularity/3.9.5
+GPUS=8
 
 echo Training coreference model
 mkdir -p $HOME/.allennlp
-singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ${coref_config_file} -s $1
+
+train () {
+    config_file=$1
+    out=$2
+    cnt=$3
+    mkdir model/$2
+    SINGULARITYENV_CUDA_VISIBLE_DEVICES=$(((cnt - 1) % GPUS)) singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ./coref_config/$config_file -s ./model/$out &
+    if [[ cnt%GPUS -eq 0 ]]
+    then
+        wait
+    fi
+}
+ 
+i=1
+for j in $(seq 10):
+    train w2v_original w2v_original$j $i
+    i=$((i + 1))
+
+for wordlist in winobias weat7
+do
+    for typ in debias overbias
+    do
+        for sample_prob in $(seq 0.0 0.1 0.9)
+        do
+            temp=w2v_db_${wordlist}_${typ}_$sample_prob
+            train $temp $temp $i
+            i=$((i + 1))
+        done
+    done
+
+    for type in debias overbias
+    do
+        for reg in 1e-1 5e-2 1e-2
+        do
+            for sim in 0.0 0.5 1.0
+            do
+                for ant in 0.0 0.5 1.0
+                do
+                    temp=w2v_ar_${wordlist}_${type}_reg${reg}_sim${sim}_ant${ant}
+                    train $temp $temp $i
+                    i=$((i + 1))
+                done
+            done
+        done
+    done
+done
+wait
--- coref/coref_train_original.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Training coreference model
-mkdir -p $HOME/.allennlp
-
-i=1
-for i in $(seq 10):
-    mkdir model/w2v_original$i
-    singularity run -e --pwd $HOME/in-ex-correlation/coref --bind $HOME/.allennlp:/root/.allennlp --nv models_latest.sif train ./coref_config/w2v_original -s ./model/w2v_original$i
