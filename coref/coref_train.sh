769c9f5c9c7e0070315bba2c9963e6a6e96d7a20
Katô Taisei <katotaisei064@gmail.com>
2023-09-13 09:51:01 +0900

    train coref models based on fasttext

--- coref/coref_train.sh
+++ coref/coref_train.sh
@@ -18,33 +18,36 @@ train () {
 }
  
 i=1
-for j in $(seq 10):
-    train w2v_original w2v_original$j $i
-    i=$((i + 1))
-
-for wordlist in winobias weat7
+for word_emb in w2v ft
 do
-    for typ in debias overbias
-    do
-        for sample_prob in $(seq 0.0 0.1 0.9)
-        do
-            temp=w2v_db_${wordlist}_${typ}_$sample_prob
-            train $temp $temp $i
-            i=$((i + 1))
-        done
-    done
+    for j in $(seq 10):
+        train ${word_emb}_original ${word_emb}_original$j $i
+        i=$((i + 1))
 
-    for type in debias overbias
+    for wordlist in winobias weat7
     do
-        for reg in 1e-1 5e-2 1e-2
+        for typ in debias overbias
         do
-            for sim in 0.0 0.5 1.0
+            for sample_prob in $(seq 0.0 0.1 0.9)
             do
-                for ant in 0.0 0.5 1.0
+                temp=${word_emb}_db_${wordlist}_${typ}_$sample_prob
+                train $temp $temp $i
+                i=$((i + 1))
+            done
+        done
+
+        for type in debias overbias
+        do
+            for reg in 1e-1 5e-2 1e-2
+            do
+                for sim in 0.0 0.5 1.0
                 do
-                    temp=w2v_ar_${wordlist}_${type}_reg${reg}_sim${sim}_ant${ant}
-                    train $temp $temp $i
-                    i=$((i + 1))
+                    for ant in 0.0 0.5 1.0
+                    do
+                        temp=${word_emb}_ar_${wordlist}_${type}_reg${reg}_sim${sim}_ant${ant}
+                        train $temp $temp $i
+                        i=$((i + 1))
+                    done
                 done
             done
         done
