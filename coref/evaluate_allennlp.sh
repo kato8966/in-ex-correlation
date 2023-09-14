8d3a7a7d26f21b4ceb0c7ab4af4ecc8619901f1f
Katô Taisei <katotaisei064@gmail.com>
2023-09-14 11:13:56 +0900

    evaluate ft coref

--- coref/evaluate_allennlp.sh
+++ coref/evaluate_allennlp.sh
@@ -11,39 +11,42 @@ eval_allennlp ()
     allennlp evaluate ./model/$1/model.tar.gz ./evaluation_data/test_type2_pro_stereotype.v4_auto_conll --output-file ./result/$1/type2_pro_results.txt
 }
 
-for i in $(seq 10)
+for word_emb in w2v ft
 do
-    eval_allennlp w2v_original$i &
-done
-wait
-
-for wordlist in winobias weat7
-do
-    for type in debias overbias
+    for i in $(seq 10)
     do
-        for ratio in $(seq 0.0 0.1 0.9)
-        do
-            eval_allennlp w2v_db_${wordlist}_${type}_$ratio &
-        done
-        wait
+        eval_allennlp ${word_emb}_original$i &
     done
+    wait
 
-    t=1
-    for type in debias overbias
+    for wordlist in winobias weat7
     do
-        for reg in 1e-1 5e-2 1e-2
+        for type in debias overbias
         do
-            for sim in 0.0 0.5 1.0
+            for ratio in $(seq 0.0 0.1 0.9)
             do
-                for ant in 0.0 0.5 1.0
+                eval_allennlp ${word_emb}_db_${wordlist}_${type}_$ratio &
+            done
+            wait
+        done
+
+        t=1
+        for type in debias overbias
+        do
+            for reg in 1e-1 5e-2 1e-2
+            do
+                for sim in 0.0 0.5 1.0
                 do
-                    temp=${wordlist}_${type}_reg${reg}_sim${sim}_ant${ant}
-                    eval_allennlp w2v_ar_$temp &
-                    if [[ t%9 -eq 0 ]]
-                    then
-                        wait
-                    fi
-                    t=$((t + 1))
+                    for ant in 0.0 0.5 1.0
+                    do
+                        temp=${wordlist}_${type}_reg${reg}_sim${sim}_ant${ant}
+                        eval_allennlp ${word_emb}_ar_$temp &
+                        if [[ t%9 -eq 0 ]]
+                        then
+                            wait
+                        fi
+                        t=$((t + 1))
+                    done
                 done
             done
         done
