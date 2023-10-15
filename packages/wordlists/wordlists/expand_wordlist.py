ddd005aefeb68c246be5a5dd99fafabccdbb3236
Katô Taisei <katotaisei064@gmail.com>
2023-10-15 19:21:31 +0900

    edit expand_wordlist.py

--- packages/wordlists/wordlists/expand_wordlist.py
+++ packages/wordlists/wordlists/expand_wordlist.py
@@ -1,70 +1,58 @@
-import argparse
-import sys
+import numpy as np
 import spacy
-from .wordlists import weat_6, weat_7, weat_8
+
+from wefe.query import Query
+import wordlists
+
+N = 100
 
 
-def setup_argparse():
-    p = argparse.ArgumentParser()
-    p.add_argument('-c', dest='config_file', default='../config/zotero.yaml',
-                   help='a yaml config containing necessary API information')
-    #p.add_argument('-d', dest='output_dir', default='../outputs/D4/', help='dir to write output summaries to')
-    #p.add_argument('-m', dest='model_path', default='', help='path for a pre-trained embedding model')
-    return p.parse_args()
+def cos_sim(x, y):
+    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
 
+
+def expand_wordlist(wordlist, query, vocab):
+    exp = set()
+    emb = vocab.vectors
+    strings = vocab.strings
+    for word in wordlist:
+        vector_w = np.mean([emb[token] for token in word.split()], 0)
+        neighbors = []
+        for u in strings:
+            if strings[u] in emb:
+                vector_u = emb[u]
+                neighbors.append((cos_sim(vector_w, vector_u), u.lower()))
+        neighbors.sort(reverse=True)
+        assert len(neighbors) >= N
+        for (_, neighbor) in neighbors[:N]:
+            if (neighbor not in query.target_sets[0]
+                and neighbor not in query.target_sets[1]
+                and neighbor not in query.attribute_sets[0]
+                and neighbor not in query.attribute_sets[1]):
+                exp.add(neighbor)
+    return wordlist + list(exp)
+
+
+def expand_query(query, vocab):
+    return Query(target_sets=[expand_wordlist(target_set, query, vocab)
+                              for target_set in query.target_sets],
+                 attribute_sets=[expand_wordlist(attribute_set, query, vocab)
+                                 for attribute_set in query.attribute_sets],
+                 target_sets_names=[f'exp. {target_set_name}'
+                                    for target_set_name in query.target_sets_names],
+                 attribute_sets_names=[f'exp. {attribute_set_name}'
+                                       for attribute_set_name in query.attribute_sets_names])
 
 
 if __name__ == "__main__":
-    args = setup_argparse()
-
-    nlp = spacy.load("en_vectors_web_lg")
-
-    # if using one vector have to reshape it to 1,300 instead of just 300 vector.reshape(1,300)
-    # union of terms in WEAT 6,7,8
-    career = ["executive", "management", "professional", "corporation", "salary", "office",
-              "business", "career"]
-    career_exp = ['department', 'offices', 'employment', 'corporations', 'industry', 'subsidiary',
-                  'expert', 'manage', 'paid', 'stint', 'success', 'corp', 'wages', 'vice', 'wage',
-                  'entity', 'staff', 'desk', 'income', 'enterprise', 'careers', 'managers',
-                  'marketing', 'chief', 'companies', 'successful', 'job', 'accounting',
-                  'vice-president', 'appointed', 'services', 'headquarters', 'corp.', 'managment',
-                  'appointment', 'competent', 'salaries', 'development', 'professionals',
-                  'corporate', 'earning', 'president', 'knowledgeable', 'coaching',
-                  'professionally', 'executives', 'qualified', 'skilled', 'subsidiaries', 'pay',
-                  'director', 'experienced', 'businesses', 'managing', 'payroll', 'consulting', 'company']
-
-    family = ["home", "parents", "children", "family", "cousins", "marriage", "wedding",
-              "relatives"]
-    family_exp = ['houses', 'house', 'friends', 'kids', 'siblings', 'babies',
-                  'marital', 'families', 'in-laws', 'apartment', 'prom', 'toddlers', 'marry',
-                  'weddings', 'grandparents', 'infants', 'cousin', 'child', 'nieces']
-    art = ['art', 'poetry', 'symphony', 'drama', 'dance', 'Shakespeare', 'sculpture', 'literature',
-           'novel']
-    art_exp = ['symphonic', 'romance', 'adaptation', 'shakespearean', 'poetic', 'dances',
-               'philharmonic', 'prose', 'music', 'choral', 'poems', 'artworks', 'orchestral',
-               'artistic', 'poem', 'sculptures', 'arts', 'novels', 'melodrama', 'choreography',
-               'literary', 'fiction', 'novella', 'paintings', 'dancers', 'painting']
-    match_sci = ['chemistry', 'math', 'science', 'computation', 'Einstein', 'experiment', 'algebra',
-                 'geometry', 'NASA', 'astronomy', 'physics', 'numbers', 'technology', 'equations',
-                 'addition', 'calculus']
-    math_sci_exp = ['innovations', 'sciences', 'biochemistry', 'technologies', 'astrophysics',
-                    'algorithms', 'experimenting', 'algorithm', 'nonlinear', 'computations',
-                    'astronomers', 'quadratic', 'arithmetic', 'experimental', 'astronomical',
-                    'digits', 'counting', 'telescope', 'count', 'research', 'molecular', 'innovation',
-                    'innovative', 'idea', 'chemical', 'technological', 'biology', 'scientific',
-                    'mathematical', 'geometric', 'high-tech', 'symmetry', 'telescopes',
-                    'approximation', 'astronomer', 'numerical', 'laboratory', 'computational',
-                    'maths', 'cosmology', 'microbiology','quantum', 'geometries', 'equation',
-                    'number', 'theory', 'compute', 'experimented', 'geometrical', 'percentages',
-                    'percentage', 'trigonometry', 'relativity', 'experimentation', 'algebraic',
-                    'mathematics', 'experiments', 'calculations', 'calculation']
-    male = ['grandfather', 'uncle', 'son', 'boy', 'father', 'he', 'him', 'his', 'man', 'male',
-            'brother']
-    male_exp = ['guy', 'himself', 'nephew', 'grandson', 'men', 'boys', 'great-grandfather',
-                'father-in-law', 'husband', 'brothers', 'males', 'brother-in-law', 'sons', 'dad']
-    female = ['daughter', 'she', 'her', 'grandmother', 'mother', 'aunt', 'sister', 'hers', 'woman',
-              'female', 'girl']
-    female_exp = ['grandma', 'herself', 'sister-in-law', 'niece', 'sisters', 'mom', 'mother-in-law',
-                  'lady', 'wife', 'females', 'girls', 'great-grandmother', 'women', 'sexy', 'granddaughter', 'daughters']
-
-
+    nlp = spacy.load("en_core_web_lg")
+    vocab = nlp.vocab
+    print('WinoBias wordlist expansion:')
+    print(expand_query(wordlists.winobias(), vocab))
+    print()
+    print('Hate speech wordlist expansion:')
+    print(expand_query(wordlists.hatespeech(), vocab))
+    print()
+    print('WEAT wordlist expansion:')
+    print(expand_query(wordlists.weat_all(), vocab))
+    print()
