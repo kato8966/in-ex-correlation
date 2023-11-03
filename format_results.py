6c23204bf0220def57354f0ec6dc0319619a5e39
Katô Taisei <katotaisei064@gmail.com>
2023-11-03 16:31:39 +0900

    edit and run format_results.py

--- format_results.py
+++ format_results.py
@@ -1,52 +1,59 @@
 import csv
 import json
-import logging
-import math
 import os
 
 for task in ['coref', 'hatespeech']:
-    HEADERS = ['name', 'weat_es']
     if task == 'coref':
-        wordlists = ['winobias', 'weat7']
-        HEADERS += [f'type{typ}_{metric}_diff' for typ in [1, 2]
-                    for metric in ['precision', 'recall', 'f1']]\
-                        + ['CoNLL F1']
+        bias_modification_wordlists = ['winobias', 'weat_gender']
     else:
-        wordlists = ['hatespeech', 'weat8']
-        HEADERS += [f'{metric}_diff'
-                    for metric in ['precision', 'recall', 'f1']]
+        bias_modification_wordlists = ['hatespeech', 'weat_gender']
     for word_emb in ['w2v', 'ft']:
-        for wordlist in wordlists:
+        for bias_modification_wordlist in bias_modification_wordlists:
+            HEADERS = ['name']
+            if bias_modification_wordlist != 'weat_gender':
+                bias_eval_wordlists = [bias_modification_wordlist]
+            elif task == 'coref':
+                bias_eval_wordlists = ['weat_6', 'weat_7', 'weat_8']
+            else:
+                bias_eval_wordlists = ['weat_6', 'weat_7_twitter', 'weat_8']
+            for bias_eval_wordlist in bias_eval_wordlists:
+                HEADERS.append(f'weat_es_{bias_eval_wordlist}')
+            if task == 'coref':
+                HEADERS += ([f'type{typ}_{metric}_diff' for typ in [1, 2]
+                             for metric in ['precision', 'recall', 'f1']]
+                            + ['CoNLL F1'])
+            else:
+                HEADERS += [f'{metric}_diff'
+                            for metric in ['precision', 'recall', 'f1']]
+
             with open(os.path.join('results',
-                                   f'{task}_{word_emb}_{wordlist}.csv'), 'w',
-                      newline='') as csvout:
+                                   f'{task}_{word_emb}_{bias_modification_wordlist}.csv'),
+                      'w', newline='') as csvout:
                 csv_writer = csv.writer(csvout)
                 csv_writer.writerow(HEADERS)
-                names = [f'original{i}' for i in range(1, 11)]\
-                        + [f'db_{wordlist}_{typ}_{sample_prob}'  # noqa: E127
-                           for typ in ['debias', 'overbias']
-                           for sample_prob in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
-                                               0.6, 0.7, 0.8, 0.9]]\
-                        + [f'ar_{wordlist}_{typ}_reg{reg}_sim{sim}_ant{ant}'
-                           for typ in ['debias', 'overbias']
-                           for reg in ['1e-1', '5e-2', '1e-2']
-                           for sim in [0.0, 0.5, 1.0] for ant in [0.0, 0.5, 1.0]]  # noqa: E501
+                names = ([f'original{i}' for i in range(1, 11)]
+                         + [f'db_{bias_modification_wordlist}_{typ}_{sample_prob}'  # noqa: E127
+                            for typ in ['debias', 'overbias']
+                            for sample_prob in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
+                                                0.6, 0.7, 0.8, 0.9]]
+                         + [f'ar_{bias_modification_wordlist}_{typ}_reg{reg}_sim{sim}_ant{ant}'
+                            for typ in ['debias', 'overbias']
+                            for reg in ['1e-1', '5e-2', '1e-2']
+                            for sim in [0.0, 1.0] for ant in [0.0, 1.0]])
                 for name in names:
                     if task == 'coref':
                         corpus = 'wikipedia'
                     else:
                         corpus = 'twitter'
                     if 'original' in name:
-                        weat_file_name = f'{corpus}_{word_emb}_{wordlist}'
+                        weat_file_name = f'{corpus}_{word_emb}_{bias_modification_wordlist}'
                     else:
                         weat_file_name = f'{corpus}_{word_emb}_{name}'
                     with open(os.path.join('WEAT', 'result',
                                            weat_file_name + '.txt')) as weatin:
-                        weat = json.load(weatin)['effect_size']
-
-                    if math.isnan(weat):
-                        logging.warning(f'{name} has nan value, thus skipped.')
-                        continue
+                        result = json.load(weatin)
+                        weat_es = [result[bias_eval_wordlist]['effect_size']
+                                   for bias_eval_wordlist in bias_eval_wordlists]
 
                     if task == 'coref':
                         coref = {}
@@ -66,7 +73,7 @@ for task in ['coref', 'hatespeech']:
                             for metric in ['precision', 'recall', 'f1']:
                                 coref[f'type{typ}_{metric}_diff'] = coref[f'type{typ}_pro_{metric}'] - coref[f'type{typ}_anti_{metric}']  # noqa: E501
 
-                        csv_writer.writerow([name, weat]
+                        csv_writer.writerow([name] + weat_es
                                             + [coref[f'type{typ}_{metric}_diff']
                                                for typ in [1, 2]
                                                for metric in ['precision',
@@ -79,7 +86,7 @@ for task in ['coref', 'hatespeech']:
                             result = json.load(hatespeechin)
                             for metric in ['precision', 'recall', 'f1']:
                                 hatespeech[f'{metric}_diff'] = result['male'][metric] - result['female'][metric]  # noqa: E501
-                        csv_writer.writerow([name, weat]
+                        csv_writer.writerow([name] + weat_es
                                             + [hatespeech[f'{metric}_diff']
                                                for metric in ['precision',
                                                               'recall', 'f1']])
