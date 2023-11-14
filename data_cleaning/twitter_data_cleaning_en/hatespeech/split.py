e2b7870b7255ea2295809e778c0408defe10bc18
Katô Taisei <katotaisei064@gmail.com>
2023-11-14 09:55:54 +0900

    split hate speech dataset by race

--- data_cleaning/twitter_data_cleaning_en/hatespeech/split.py
+++ data_cleaning/twitter_data_cleaning_en/hatespeech/split.py
@@ -1,5 +1,3 @@
-import re
-
 import pandas as pd
 
 data = pd.read_csv('hatespeech_text_label_vote_RESTRICTED_100K.csv', sep='\t',
@@ -13,7 +11,17 @@ for section in ['train', 'val']:
         section_data.to_csv(f'hate_{section}.tsv', '\t', index=False)
 
 gender_ids = pd.read_csv('hate_test_gender_id.csv')
-for gender in ['male', 'female', 'neutral']:
+test_ids = list(gender_ids['id'])
+test_data = data.iloc[test_ids]
+test_data.to_csv('hate_test.tsv', '\t', index=False)
+
+for gender in ['male', 'female']:
     ids = list(gender_ids['id'].loc[gender_ids['gender'] == gender])
     test_data = data.iloc[ids]
     test_data.to_csv(f'hate_test_{gender}.tsv', '\t', index=False)
+
+race_ids = pd.read_csv('hate_test_race_id.csv')
+for race in ['aa', 'w']:
+    ids = list(race_ids['id'].loc[race_ids['race'] == race])
+    test_data = data.iloc[ids]
+    test_data.to_csv(f'hate_test_{race}.tsv', '\t', index=False)
