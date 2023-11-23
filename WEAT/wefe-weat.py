46540329ff61973551f774a7151091c70cff794a
Katô Taisei <katotaisei064@gmail.com>
2023-11-24 07:37:52 +0900

    measure WEAT for hatespeech_race

--- WEAT/wefe-weat.py
+++ WEAT/wefe-weat.py
@@ -57,7 +57,8 @@ if __name__ == '__main__':
                                                         f'result/{temp}.txt'))
 
             for (bias_modification_wordset, bias_eval_wordsets) in [('hatespeech_gender', ['hatespeech_gender']),
-                                                                    ('weat_gender', ['weat_6', 'weat_7_twitter', 'weat_8'])]:
+                                                                    ('weat_gender', ['weat_6', 'weat_7_twitter', 'weat_8']),
+                                                                    ('hatespeech_race', ['hatespeech_race'])]:
                 futures.append(p.submit(main, f'../{word_emb}/vectors/'
                                               'twitter.txt',
                                         bias_eval_wordsets,
